546e6a911c1a9ce3311c9d82d0884b079cd722c5
Walker Sorensen <wsoren1@mc2school.org>
2019-07-27 23:06:45 -0400

    Setup File complete

--- setup.py
+++ setup.py
@@ -6,14 +6,20 @@ if input('Are you sure? If you have data in these files, it will be completely o
         pass
 
     with open('slack_credentials.json', 'w') as scfile:
-        sc_template = '{"slack_api_key":"None", "active_channel":"None"}'
+        sc_template = '{"slack_api_key":"None",\n"active_channel":"None"}'
         scfile.write(sc_template)
 
     with open('trello_credentials.json', 'w') as trfile:
-        tr_template = '{"trello_api_key":"None", "trello_token":"None", ' \
-                      '"trello_board_id":"None", "trello_list_id":"None", ' \
+        tr_template = '{"trello_api_key":"None",\n"trello_token":"None",\n' \
+                      '"trello_board_id":"None",\n"trello_list_id":"None",\n' \
                       '"class_label_id":"None"}'
         trfile.write(tr_template)
 
     with open('settings.json', 'w') as setfile:
-        pass
+        settings_template = '{"timeout":30,\n'\
+                            '"bot_user":"jarvis",\n'\
+                            '"bot_icon":":satellite:",\n'\
+                            '"timedelta_assignment":3,\n'\
+                            '"refresh_time":10}'
+        setfile.write(settings_template)
+
